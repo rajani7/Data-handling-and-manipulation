4bafd2b4015bafc3618363164b662b5068530e12
rajani <rajanikulkarni84@gmail.com>
2024-09-07 22:01:09 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("joey");
+  const friendsData = [
+    { id: 0, name: "joey", quote: "How you doin?" },
+    { id: 1, name: "ross", quote: "We were on a break" },
+    { id: 2, name: "phoebe", quote: "She’s your lobster" },
+    { id: 3, name: "monica", quote: "Damn the jellyfish!" },
+    { id: 4, name: "chandler", quote: "I'm not great at advice" },
+    { id: 5, name: "rachel", quote: "It’s a metaphor, Daddy." },
+  ];
+  const options = [
+    { value: "joey", label: "joey" },
+    { value: "ross", label: "ross" },
+    { value: "phoebe", label: "phoebe" },
+    { value: "monica", label: "monica" },
+    { value: "chandler", label: "chandler" },
+    { value: "rachel", label: "rachel" },
+  ];
+
+  const handleButtonChange = () => {
+    alert(`${value} is selected`);
+  };
+
+  //Function to return the particular object depending on name that is passed as parameter
+  const returnObject = (name) => {
+    const returnedValue = friendsData.filter((data) => {
+      return data.name === name;
+    });
+    return returnedValue;
+  };
+  console.log("Object returned: ", returnObject("joey"));
+
+  //To display friends data which is array of objects in list format
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h2>Data Handling and Data Manipulation</h2>
+      <div className="data-list">
+        {friendsData.map((data) => (
+          <div className="data-items" key={data.id}>
+            {`${data.name} - ${data.quote} `}
+          </div>
+        ))}
+      </div>
+      <div className="dropdown-section">
+        <select className="dropdown-list" onChange={(e)=> {setValue(e.target.value)}}>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <button
+          className="dropdown-sec-btn"
+          type="button"
+          onClick={handleButtonChange}
         >
-          Learn React
-        </a>
-      </header>
+          Click Me
+        </button>
+      </div>
     </div>
   );
 }
